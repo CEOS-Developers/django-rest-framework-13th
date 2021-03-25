9a5c414577eb6f1d77c76e72a8d6f021bd0f4257
jaethewiederholen <herakjy0705@gmail.com>
2021-03-25 12:54:02 +0900

    modify profile table

--- api/models.py
+++ api/models.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
-from django.contrib.auth.models import User
-
+from django.contrib.auth.models import User, AbstractUser
 
 # Create your models here.
+from django.db.models import CheckConstraint
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=32, default='')
-    phone = models.CharField(max_length=32)
+    phone = models.CharField(max_length=32, null=True)
     email = models.EmailField(max_length=254, null=True)
     bio = models.TextField(max_length=500, blank=True, null=True)
     website = models.TextField(max_length=100, blank=True, null=True)
@@ -15,6 +19,24 @@ class Profile(models.Model):
     gender = models.CharField(max_length=10, null=True)
     birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to="profileImages", null=True)
+
+    # email and phone cannot be both null
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=(models.Q(phone__isnull=False) | models.Q(email__isnull=False)),
+                name='not_both_null'
+            )
+        ]
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 
 # related name is used to make two fks from one table
