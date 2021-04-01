cfdd99c921623bf0cb3e66ffca7a8172a4d4be19
최성민 <csm1165@naver.com>
2021-04-01 22:57:49 +0900

    Added registers of models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Post, Media, PeopleTag, HashTag
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Media)
+admin.site.register(PeopleTag)
+admin.site.register(HashTag)
