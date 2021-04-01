d3ad9b4ded21b11cec93e56c3d2642be7ad4acf7
최성민 <csm1165@naver.com>
2021-04-02 00:17:43 +0900

    Added ModelAdmin to show fields in admin page

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
 from .models import Profile, Post, Media, PeopleTag, HashTag
 
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(Media)
-admin.site.register(PeopleTag)
-admin.site.register(HashTag)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Profile._meta.fields]
+
+
+admin.site.register(Profile, ProfileAdmin)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Post._meta.fields]
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class MediaAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Media._meta.fields]
+
+
+admin.site.register(Media, MediaAdmin)
+
+
+class PeopleTagAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in PeopleTag._meta.fields]
+
+
+admin.site.register(PeopleTag, PeopleTagAdmin)
+
+
+class HashTagAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in HashTag._meta.fields]
+
+
+admin.site.register(HashTag, HashTagAdmin)
