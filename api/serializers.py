f90838416245321ed5fc453a2195075dd71d521f
최성민 <csm1165@naver.com>
2021-04-01 17:43:01 +0900

    Made serializers.py

---
+++ api/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Profile, Post, Media, PeopleTag, HashTag
+
+
+class PeopleTagSerializer(serializers.ModelSerializer):  # PeopleTag model serializer
+    class Meta:
+        model = PeopleTag
+        fields = '__all__'  # all fields in PeopleTag model
+
+
+class HashTagSerializer(serializers.ModelSerializer):  # HashTag model serializer
+    class Meta:
+        model = HashTag
+        fields = '__all__'  # all fields in HashTag model
+
+
+class MediaSerializer(serializers.ModelSerializer):  # Media model serializer
+    peopletags = PeopleTagSerializer(many=True)
+
+    class Meta:
+        model = Media
+        fields = ['upload', 'subs_text', 'peopletags']  # all fields in Media and PeopleTag model
+
+
+class PostSerializer(serializers.ModelSerializer):  # Post model serializer
+    medias = MediaSerializer(many=True)  # Post - Media : ForeignKey
+    hashtags = HashTagSerializer(many=True)  # Post - Hashtag : ForeignKey
+
+    class Meta:
+        model = Post
+        fields = ['pub_date', 'content', 'location', 'location', 'ratio', 'comment_permission', 'medias', 'hashtags']
+        # all fields in Post, Media and Hashtag model
+
+
+class ProfileSerializer(serializers.ModelSerializer):  # Profile model serializer
+    posts = PostSerializer(many=True)  # Profile - Post : ForeignKey
+
+    class Meta:
+        model = Profile
+        fields = ['user', 'name', 'nickname', 'intro', 'profile_image', 'website', 'email', 'phone', 'birthday',
+                  'gender', 'posts']  # all fields in Profile and Post model
+
+
+class UserSerializer(serializers.ModelSerializer):  # User model serializer
+    profile = ProfileSerializer()  # User - Profile : OneToOneField
+
+    class Meta:
+        model = User
+        fields = 'username'  # username field 만 serialize
