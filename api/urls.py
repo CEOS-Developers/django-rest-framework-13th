9dd36b7e0dc397bb01c44ee1594feb78a91e769e
최성민 <csm1165@naver.com>
2021-04-03 21:11:53 +0900

    Changed path

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import UserList, PostList
+from .views import UserViewSet, PostViewSet
+
+user_list = UserViewSet.as_view({
+    'get': 'list',
+    'put': 'create'
+})
+
+post_list = PostViewSet.as_view({
+    'get': 'list',
+    'put': 'create'
+})
 
 urlpatterns = [
-    path('api/users', UserList.as_view()),
-    path('api/posts', PostList.as_view())
+    path('api/users', user_list, name='user-list'),
+    path('api/posts', post_list, name='post-list')
 ]
