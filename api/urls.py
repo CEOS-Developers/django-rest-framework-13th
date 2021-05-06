b88b47846976d87e60242dd7faa604ecd98695b3
gimkuku <khjyj125@gmail.com>
2021-05-06 12:46:32 +0900

    readme 수정

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('post/', views.postList.as_view()),
-    path('post/<int:pk>', views.postDetail.as_view())
-]
+    path('post/', views.PostList.as_view()),
+    path('post/<int:pk>', views.PostDetail.as_view())
+    ]
