8439c07c500b9724074bbdd41764cb6c82a414a3
최성민 <csm1165@naver.com>
2021-05-13 16:03:17 +0900

    Modify views.py by using ViewSets

--- api/views.py
+++ api/views.py
@@ -1,81 +1,81 @@
 from django.contrib.auth.models import User
 from api.models import Post
 from api.serializers import UserSerializer, PostSerializer
-from rest_framework.generics import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status  # , viewsets
+# from rest_framework.generics import get_object_or_404
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+from rest_framework import viewsets  # , status
 
 
-class UserList(APIView):
-    def get(self, request, format=None):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
+# class UserList(APIView):
+#     def get(self, request, format=None):
+#         users = User.objects.all()
+#         serializer = UserSerializer(users, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = UserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class PostList(APIView):
-
-    # GET REQUEST 를 받는다.
-    # Post model 의 모든 instance 를 불러오는 method
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-
-    # POST REQUEST 를 받는다.
-    # Post model 에 새로운 instance 를 불러오는 method
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-# class PostViewSet(viewsets.ModelViewSet):
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
+# class PostList(APIView):
+#
+#     # GET REQUEST 를 받는다.
+#     # Post model 의 모든 instance 를 불러오는 method
+#     def get(self, request):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(posts, many=True)
+#         return Response(serializer.data)
+#
+#     # POST REQUEST 를 받는다.
+#     # Post model 에 새로운 instance 를 불러오는 method
+#     def post(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class PostDetail(APIView):
 
-    # arg 에 들어가는 pk 와 같은 pk의 instance가 Post model에 존재하는 지 확인한다.
-    # 존재할 경우 해당 instance를 불러오고 그렇지 않을 경우 404 error를 발생시킨다.
-    def get_object(self, pk):
-        return get_object_or_404(Post, pk=pk)
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-    # GET REQUEST 를 받는다.
-    # Post model 의 pk 번 instance 를 불러오는 method
-    def get(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-
-    # PUT REQUEST 를 받는다.
-    # Post model 의 pk 번 instance 를 수정하는 method
-    def put(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # DELETE REQUEST 를 받는다.
-    # Post model 의 pk 번 instance 를 삭제하는 method
-    def delete(self, request, pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# class PostDetail(APIView):
+#
+#     # arg 에 들어가는 pk 와 같은 pk의 instance가 Post model에 존재하는 지 확인한다.
+#     # 존재할 경우 해당 instance를 불러오고 그렇지 않을 경우 404 error를 발생시킨다.
+#     def get_object(self, pk):
+#         return get_object_or_404(Post, pk=pk)
+#
+#     # GET REQUEST 를 받는다.
+#     # Post model 의 pk 번 instance 를 불러오는 method
+#     def get(self, request, pk):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#
+#     # PUT REQUEST 를 받는다.
+#     # Post model 의 pk 번 instance 를 수정하는 method
+#     def put(self, request, pk):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     # DELETE REQUEST 를 받는다.
+#     # Post model 의 pk 번 instance 를 삭제하는 method
+#     def delete(self, request, pk):
+#         post = self.get_object(pk)
+#         post.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
