ace1191032bb3c6e87cf51f50cef97d8adf7627d
최성민 <csm1165@naver.com>
2021-05-13 16:04:13 +0900

    Modified urlpatterns by applying router

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-# from .views import UserViewSet, PostViewSet
-from .views import UserList, PostList, PostDetail
+from .views import UserViewSet, PostViewSet
+from rest_framework.routers import DefaultRouter
+
+# from .views import UserList, PostList, PostDetail
 
 # user_list = UserViewSet.as_view({
 #     'get': 'list',
@@ -12,8 +14,14 @@ from .views import UserList, PostList, PostDetail
 #     'post': 'create'
 # })
 
-urlpatterns = [
-    path('api/users', UserList.as_view(), name='user-list'),
-    path('api/posts', PostList.as_view(), name='post-list'),
-    path('api/posts/<int:pk>', PostDetail.as_view(), name='post-detail')
-]
+router = DefaultRouter()
+router.register(r'user', UserViewSet)
+router.register(r'posts', PostViewSet)
+
+# urlpatterns = [
+#     path('api/users', UserList.as_view(), name='user-list'),
+#     path('api/posts', PostList.as_view(), name='post-list'),
+#     path('api/posts/<int:pk>', PostDetail.as_view(), name='post-detail')
+# ]
+
+urlpatterns = router.urls
